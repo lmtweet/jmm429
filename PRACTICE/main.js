848b2d5d9dfeed28c9020bc97720002787c2d5dc
lmtweet <58784879+lmtweet@users.noreply.github.com>
2020-02-17 19:57:04 -0500

    practicing javascript

---
+++ PRACTICE/main.js
@@ -0,0 +1,27 @@
+let x;
+let y;
+let z;
+
+x = 27;
+y = 'string';
+z = true;
+
+console.log(x + z);
+
+console.log(x +y);
+
+let myArray = [1, 'two', 3, 4, 5, 6, 7, 8, 9];
+
+
+console.log(myArray);
+console.log(myArray.length);
+
+let fruit = {
+    kind: 'orange',
+    color: 'orange',
+    quantity: 9,
+    tasty: true,
+
+};
+
+console.log(fruit);
